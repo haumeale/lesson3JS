d8cb5fab9b1c6ebd02db48757444db0d0089e146
haumeale <haumealesya@gmail.com>
2023-04-21 21:28:07 +0300

    1st lesson

---
+++ script.ts
@@ -0,0 +1,17 @@
+// console.log("hi")
+// console.log(10/0)
+
+// let h2 = document.querySelector('h2')
+// // @ts-ignore Игнорирует предупреждения в следующей строке
+// setTimeout(() => { h2.innerText = 'Safari'}, 3000)
+
+// let a  = 1
+// a = 2 + 3 + 2 
+// const daysInWeek = a
+// console.log(daysInWeek)
+
+// consolelet
+
+let admin = 'Jon'
+let name1 = 'Jon'
+console.log(admin)
