8cf4f09cc0b068c8e8fa1a924630f963bd4bc177
haumeale <haumealesya@gmail.com>
2023-04-28 21:22:36 +0300

    init

--- dist/script.js
+++ dist/script.js
@@ -12,6 +12,51 @@
 // let name1 = 'Jon'
 // console.log(admin)
 // const x = ('Введите число','0')
-console.log('typeof(NaN)'), typeof (NaN);
-console.log('typeof(15)'), typeof (15);
-console.log('type(Infinity)'), typeof (Infinity);
+// console.log('typeof(NaN)'), typeof (NaN)
+// console.log('typeof(15)'), typeof (15)
+// console.log('type(Infinity)'), typeof (Infinity)
+// console.log('4 > 3', 4 > 3)
+// console.log('4 == 3', s1 == s2)
+// console.log('4 != 3', s1 != s2)
+// console.log('"b" > "a"', "b" > "a")
+// console.log()
+1.; // let a = +prompt("введите число");
+// if  (a > 0){
+//     alert("a положительно")
+// } else if (a < 0) {
+//     alert("a отрицательно")
+// } else if (a == 0){
+//     alert("a = 0")
+// }
+2.; // let a = +prompt("введите возраст");
+// if (120 >= a) {
+//     alert("корректно")
+// } if  (120 < a) {
+//     alert("некорректно");
+// }
+// let a = +prompt("введите число");
+// if(a || (-a)){
+// alert(Math.abs(a))
+// }
+// let a = +prompt("введите часы");
+// let b = +prompt("введите минуты");
+// let c = +prompt("введите секунды");
+// if (0 < a < 24 && 0 < b < 60 && 0 < c < 60) {
+//     console.log("OK");
+// } else {
+//   console.log("error");
+// }
+var x = +prompt("координаты точки x");
+var y = +prompt("координаты точки y");
+if (x > 0 && y > 0) {
+    alert("First quarter");
+}
+else if (x < 0 && y > 0) {
+    alert("Second quarter");
+}
+else if (x < 0 && y < 0) {
+    alert("Third quarter");
+}
+else if (x > 0 && y < 0) {
+    alert("Fourth quarter");
+}
